0297014adb0474bf70a42e2845fbe4675caf929c
Shigeto Kumagai <shk@all.daynight.jp>
2025-07-29 14:19:09 +0900

    Follow other migration script style to source install script

--- migrations/1753760843.sh
+++ migrations/1753760843.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
-
-# Install batsignal
-if ! command -v batsignal &>/dev/null; then
-  yay -S --noconfirm --needed batsignal
-fi
-
-# Enable Battery warning
-# https://github.com/electrickite/batsignal
-if [[ -f ~/.config/systemd/user/batsignal.service.d/options.conf ]]; then
-  mkdir -p mkdir -p ~/.config/systemd/user/batsignal.service.d
-  printf '[Service]\nExecStart=\nExecStart=batsignal -c 10 -w 30 -f 97' >~/.config/systemd/user/batsignal.service.d/options.conf
-fi
-
-if upower -e | grep BAT &>/dev/null; then
-  if ! pidof -x batsignal &>/dev/null; then
-    systemctl --user enable batsignal.service
-    systemctl --user start batsignal.service
-  fi
-fi
+echo "Install Batsignal Battery warning"
+source "$HOME/.local/share/omarchy/install/config/battery-warning.sh"
